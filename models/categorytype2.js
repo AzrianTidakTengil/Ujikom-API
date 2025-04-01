a5f2a123491908fa5a1dc7faba747076b783fa57
Hikoyu <azrianpintar@gmail.com>
2025-04-01 20:11:36 +0700

    feat: new model category type 2

---
+++ models/categorytype2.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class CategoryType2 extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      CategoryType2.hasMany(models.ProductCategory, {
+        foreignKey: 'product_id',
+        as: 'type2ToProductCategory'
+      })
+    }
+  }
+  CategoryType2.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'CategoryType2',
+  });
+  return CategoryType2;
+};
