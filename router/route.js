64e4dc2f0ffaa5e3c3016e20b481de6cf489589f
Hikoyu <azrianpintar@gmail.com>
2025-08-14 11:01:33 +0700

    feat(route/auth): add forgot password endpoint

--- router/route.js
+++ router/route.js
@@ -13,6 +13,7 @@ module.exports = function (app) {
     app.post('/api/auth/otp', [middleware.auth.checkEmail], controllers.auth.VerifyEmail)
     app.post('/api/auth/otp/verify', [middleware.auth.ClearExpireOTP], controllers.auth.verifyOTP)
     app.post('/api/auth/openshop', [middleware.verify.verifyToken], controllers.auth.ChangeToShop)
+    app.post('/api/auth/forgotpassword', [], controllers.auth.ForgotPassword)
 
     // user
     app.get('/api/user', [middleware.verify.verifyToken], controllers.user.GetOne)
